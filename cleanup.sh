c50da7eaf544b87bbb56634a9a474ff4cbd7d47b
Miller, Andrew | Andrew | ADPDE <andrew.miller@rakuten.com>
2023-01-05 12:40:58 +0900

    Revert path

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CID_FILE=$RUNNER_TEMP/mongodb-$GITHUB_RUN_ID.cid
+CID_FILE=$GITHUB_WORKSPACE/mongodb-$GITHUB_RUN_ID.cid
 
 docker container kill $(cat $CID_FILE)
 docker container rm --force $(cat $CID_FILE)
